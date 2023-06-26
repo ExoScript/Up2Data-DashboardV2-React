da04adb28cae6dbca67ce688dc1c18eededa31f5
ExoScript <126365301+ExoScript@users.noreply.github.com>
2023-06-26 16:00:25 -0300

    Commit from TeleportHQ

--- src/components/login-component.js
+++ src/components/login-component.js
@@ -1,134 +1,178 @@
 import React, { useState } from 'react'
 
+import { Player } from '@lottiefiles/react-lottie-player'
+
 import './login-component.css'
 
 const LoginComponent = (props) => {
   const [notification, setNotification] = useState(false)
   const [remember_me, setRemember_me] = useState(false)
+  const [loading, setLoading] = useState(false)
   return (
-    <div className="login-component-login-component box-shadow">
-      <div className="login-component-container border-b">
-        <div className="login-component-container01 gradient">
-          <div className="login-component-container02">
-            <span className="login-component-text">Login</span>
-            <span className="login-component-text01 visibility_60">now</span>
+    <div className="login-component-login-component">
+      <div className="login-component-container box-shadow">
+        <div className="login-component-container01 border-b">
+          <div className="login-component-container02 gradient">
+            <div className="login-component-container03">
+              <span className="login-component-text">Login</span>
+              <span className="login-component-text01 visibility_60">now</span>
+            </div>
           </div>
-        </div>
-        <span className="visibility_70">
-          <span>
-            Welcome to
-            <span
-              dangerouslySetInnerHTML={{
-                __html: ' ',
-              }}
-            />
+          <span className="visibility_70">
+            <span>
+              Welcome to
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span className="login-component-text04">Up2Data</span>
+            <span>! </span>
+            <span>Please login to your</span>
+            <br></br>
+            <span>account.</span>
+            <br></br>
           </span>
-          <span className="login-component-text04">Up2Data</span>
-          <span>! </span>
-          <span>Please login to your</span>
-          <br></br>
-          <span>account.</span>
-          <br></br>
-        </span>
-      </div>
-      <div className="login-component-container03">
+        </div>
         <div className="login-component-container04">
           <div className="login-component-container05">
-            <span className="login-component-text10 visibility_60">
-              Client-ID:
-            </span>
             <div className="login-component-container06">
-              <input
-                type="text"
-                placeholder="Enter your Client-ID"
-                className="login-component-textinput input"
-              />
+              <span className="login-component-text10 visibility_60">
+                Client-ID:
+              </span>
+              <div className="login-component-container07">
+                <input
+                  type="text"
+                  placeholder="Enter your Client-ID"
+                  className="login-component-textinput input"
+                />
+              </div>
+            </div>
+            <div className="login-component-container08">
+              <span className="login-component-text11 visibility_60">
+                Secret-Key:
+              </span>
+              <div className="login-component-container09">
+                <input
+                  type="text"
+                  placeholder="Enter your Secret-Key"
+                  className="login-component-textinput1 input"
+                />
+              </div>
             </div>
           </div>
-          <div className="login-component-container07">
-            <span className="login-component-text11 visibility_60">
-              Secret-Key:
-            </span>
-            <div className="login-component-container08">
-              <input
-                type="text"
-                placeholder="Enter your Secret-Key"
-                className="login-component-textinput1 input"
-              />
+          <div className="login-component-container10">
+            <div className="login-component-container11">
+              <div className="login-component-container12">
+                <div
+                  onClick={() => setRemember_me(!remember_me)}
+                  className="login-component-container13"
+                >
+                  <svg
+                    viewBox="0 0 1024 1024"
+                    className="login-component-icon icon-size"
+                  >
+                    <path d="M384 690l452-452 60 60-512 512-238-238 60-60z"></path>
+                  </svg>
+                  {remember_me && (
+                    <div className="login-component-container14"></div>
+                  )}
+                </div>
+                <span className="login-component-text12">Remember Me</span>
+              </div>
+              <span className="login-component-text13 visibility_70">
+                <span>
+                  Forget
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+                <span>Secret-Key?</span>
+              </span>
             </div>
           </div>
         </div>
-        <div className="login-component-container09">
-          <div className="login-component-container10">
-            <div className="login-component-container11">
-              <div
-                onClick={() => setRemember_me(!remember_me)}
-                className="login-component-container12"
-              >
-                <svg
-                  viewBox="0 0 1024 1024"
-                  className="login-component-icon icon-size"
-                >
-                  <path d="M384 690l452-452 60 60-512 512-238-238 60-60z"></path>
-                </svg>
-                {remember_me && (
-                  <div className="login-component-container13"></div>
-                )}
-              </div>
-              <span className="login-component-text12">Remember Me</span>
+        <div className="login-component-container15 border-t">
+          {!loading && (
+            <div className="login-component-container16 box-shadow gradient">
+              <span>Login</span>
             </div>
-            <span className="login-component-text13 visibility_70">
-              <span>
-                Forget
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
-              </span>
-              <span>Secret-Key?</span>
-            </span>
-          </div>
-          {notification && (
-            <div className="login-component-container14">
-              <span className="login-component-text16 visibility_70">
-                <span className="login-component-text17">Login failed!</span>
-                <span className="login-component-text18">
-                  <span
-                    dangerouslySetInnerHTML={{
-                      __html: ' ',
-                    }}
-                  />
-                </span>
-                <span className="login-component-text19">
-                  Please check your
-                  <span
-                    dangerouslySetInnerHTML={{
-                      __html: ' ',
-                    }}
-                  />
-                </span>
-                <span className="login-component-text20">Client-ID</span>
-                <span className="login-component-text21">
-                  {' '}
-                  and
-                  <span
-                    dangerouslySetInnerHTML={{
-                      __html: ' ',
-                    }}
-                  />
-                </span>
-                <span className="login-component-text22">Secret-Key</span>
-                <span className="login-component-text23"> and try again.</span>
-              </span>
+          )}
+          {loading && (
+            <div className="login-component-container17">
+              <Player
+                src="https://assets7.lottiefiles.com/packages/lf20_fyye8szy.json"
+                autoplay
+                speed="1"
+                background="transparent"
+                loop
+                className="login-component-lottie-node"
+              ></Player>
             </div>
           )}
         </div>
       </div>
-      <div className="login-component-container15 border-t">
-        <div className="login-component-container16 box-shadow gradient">
-          <span>Login</span>
-        </div>
+      <div className="login-component-container18 box-shadow">
+        <span className="login-component-text17 visibility_70">
+          <span className="login-component-text18">Login failed!</span>
+          <span className="login-component-text19">
+            <span>
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span>
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </span>
+          <span className="login-component-text22">
+            <span>
+              Please check your
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span>
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </span>
+          <span className="login-component-text25">Client-ID</span>
+          <span className="login-component-text26">
+            <span>
+              {' '}
+              and
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span>
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </span>
+          <span className="login-component-text29">Secret-Key</span>
+          <span className="login-component-text30"> and try again.</span>
+        </span>
       </div>
     </div>
   )
